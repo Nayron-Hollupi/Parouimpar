a64f46e5bae4f9e026658a9b46d61c4de8851fd5
Nayron Hollupi <nhollupi@gmail.com>
2022-03-07 23:06:13 -0300

    Adicionar arquivos de projeto.

---
+++ Parouimpar/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Parouimpar
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Declarar
+			int numero1 , numero2 , soma;
+
+
+			//Escrever
+			Console.WriteLine("Informe um numero inteiro : ");
+
+			//Ler
+			numero1 = Convert.ToInt32(Console.ReadLine());
+
+			//Escrever
+			Console.WriteLine("Informe um numero inteiro : ");
+
+			//Ler
+			numero2 = Convert.ToInt32(Console.ReadLine());
+
+			//Calculo
+			soma = numero1 + numero2;
+
+			//Condição Se
+			if (soma == 0)
+			{
+				//Escreva
+				Console.WriteLine("O numero {0} eh neutro!  ", soma);
+			}
+			//Senao
+			else
+			{
+				//Se
+				if (soma % 2 == 0)
+				{
+					//Escreva
+					Console.WriteLine("O numero {0} eh par!  ", soma);
+				}
+				//Senao
+				else
+				{
+					//Escreva
+					Console.WriteLine("O numero {0} eh impar!  ", soma);
+
+
+				}
+			}
+		
+			
+			Console.ReadLine();
+
+
+
+
+		}
+
+	
+	}
+}
